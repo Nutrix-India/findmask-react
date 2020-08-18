b7008c0e80216f87352ceab65e63400a8058687a
Jassu <jaswanthsaisattenapalli@gmail.com>
2020-08-18 22:34:26 +0530

    link manifest

--- src/components/composite/MetaTags.jsx
+++ src/components/composite/MetaTags.jsx
@@ -33,6 +33,26 @@ const MetaTags = () => {
       <meta name="theme-color" content={lightTheme.colors.blueGreen} />
       <meta name="twitter:site" content="@nutrix-ind" />
       <meta property="og:site_name" content="Mask Detector" />
+      {/* <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      /> */}
+      {/* <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      /> */}
+      <link rel="manifest" href="/manifest.json" />
+      {/* <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" /> */}
+      <link rel="shortcut icon" href="/favicon.ico" />
     </Head>
   );
 };
