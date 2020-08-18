b1f21ce6971f4bf14bfd431d6f767467f5a957ed
Jassu <jaswanthsaisattenapalli@gmail.com>
2020-08-19 00:45:07 +0530

    optimize pwa

--- src/components/composite/MetaTags.jsx
+++ src/components/composite/MetaTags.jsx
@@ -33,11 +33,7 @@ const MetaTags = () => {
       <meta name="theme-color" content={lightTheme.colors.blueGreen} />
       <meta name="twitter:site" content="@nutrix-ind" />
       <meta property="og:site_name" content="Mask Detector" />
-      {/* <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      /> */}
+      <link rel="apple-touch-icon" sizes="180x180" href="/logo180.png" />
       {/* <link
         rel="icon"
         type="image/png"
@@ -51,7 +47,7 @@ const MetaTags = () => {
         href="/favicon-16x16.png"
       /> */}
       <link rel="manifest" href="/manifest.json" />
-      {/* <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" /> */}
+      <link rel="mask-icon" href="/logo180.png" color="#004267" />
       <link rel="shortcut icon" href="/favicon.ico" />
     </Head>
   );
